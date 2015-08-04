f340ba1bea09e082e8ea6d18729c63fa820a22ed
martamae <wegnerma@onid.oregonstate.edu>
2015-08-03 20:34:36 -0500

    Fixed ctrl-C bug - pkill needs work

--- Prog3/smallsh.c
+++ Prog3/smallsh.c
@@ -38,17 +38,18 @@ int main() {
 	while (!exited) {
         bg = 0;
 
-		//Print prompt
+	//Print prompt
         printf(": ");     
         fflush(stdout);     
 
         //Read in a line 
-		ssize_t size = 0;
-		if (!(getline(&line, &size, stdin))) {
-			return 0;
-		}
+        ssize_t size = 0;
+	if (!(getline(&line, &size, stdin))) {
+		return 0; //end if this is the end of the file
+	}
 	//printf("line: %s\n", line);
-        //split line into tokens
+        
+	//split line into tokens
         numArgs = 0;
         token = strtok(line, "  \n");
         while (token != NULL) {
@@ -86,110 +87,110 @@ int main() {
             }
         }
 
-		//End array of args with NULL
+	//End array of args with NULL
         args[numArgs] = NULL;
         //Determine command
 		//Comment or NULL
         if (args[0] == NULL || !(strncmp(args[0], "#", 1))) {
             //if comment or null - do nothing
         } 
-		else if (strcmp(args[0], "exit") == 0) { //exit command
-            exit(0);
-			exited = 1;
+	else if (strcmp(args[0], "exit") == 0) { //exit command
+            	exit(0);
+		exited = 1;
         } 
-		else if (strcmp(args[0], "status") == 0) { //status command
-			//Print status
-			if (WIFEXITED(status)) {//print exit status
-				printf("Exit status: %d\n", WEXITSTATUS(status));
-			}
-			else { //Print terminating signal
-				printf("Terminating signal %d\n", status);
-			}
+	else if (strcmp(args[0], "status") == 0) { //status command
+		//Print status
+		if (WIFEXITED(status)) {//print exit status
+			printf("Exit status: %d\n", WEXITSTATUS(status));
 		}
-		else if (strcmp(args[0], "cd") == 0) { //cd command
-			//If there is not arg for cd
-            		if (args[1] == NULL) {
-                		chdir(getenv("HOME"));
-            		} 
-			else {//Else change to specified dir
-                		chdir(args[1]);
-            		}
-       		 }	 
-		else { //other commands
-            		//fork command
-            		pid = fork();
+		else { //Print terminating signal
+			printf("Terminating signal %d\n", status);				}
+	}
+	else if (strcmp(args[0], "cd") == 0) { //cd command
+		//If there is not arg for cd
+            	if (args[1] == NULL) {
+               		chdir(getenv("HOME"));
+           	} 
+		else {//Else change to specified dir
+                	chdir(args[1]);
+            	}
+       	}	 
+	else { //other commands
+            	//fork command
+            	pid = fork();
 
-			if(!bg) { //If the process is not in the background
-				//command can be interrupted
-				act.sa_handler = SIG_DFL;
-				act.sa_flags = 0;
-				sigaction(SIGINT, &act, NULL);
-			}
+            	if (pid == 0) {  //Child 
+ 	  	   if(!bg) { //If the process is not background
+			//command can be interrupted
+			act.sa_handler = SIG_DFL;
+			act.sa_flags = 0;
+			sigaction(SIGINT, &act, NULL);
+		   }
 
-            		if (pid == 0) {  //Child  
-                	   if (iFile != NULL) { //If file to input to
-                    		//open specified file
-                    		fd = open(iFile, O_RDONLY);
-                    		if (fd == -1) {
-					//Invalid file, exit
-                        		printf("Invalid file: %s\n", iFile);
-					fflush(stdout);
-                       			 _Exit(1);
-                   		 }
-                   		 else if (dup2(fd, 0) == -1) {
-					//Error redirecting input
-                        		perror("dup2 error");
-                       			 _Exit(1);
-                   		 }
+
+                   if (iFile != NULL) { //If file to input to
+                    	//open specified file
+                    	fd = open(iFile, O_RDONLY);
+                    	if (fd == -1) {
+				//Invalid file, exit
+                        	printf("Invalid file: %s\n", iFile);
+				fflush(stdout);
+                       		exit(1);
+                   	}
+                   	else if (dup2(fd, 0) == -1) {
+				//Error redirecting input
+                        	perror("dup2 error");
+                       		exit(1);
+                   	}
                    		
-				//close the file stream
-				 close(fd);
-                		} 
-		   	   else if (bg) {
-                    		//redirect input to /dev/null 
-                    		//if input file not specified
-                    		fd = open("/dev/null", O_RDONLY);
-                    		if (fd == -1) {
-					//error opening
-                        		perror("open error");
-                       			 _Exit(1);
-                   		 }
-                    		else if (dup2(fd, 0) == -1) {
-                        		perror("dup2 error");
-                       			 _Exit(1);
-                   		 }
+			//close the file stream
+			close(fd);
+                     } 
+		     else if (bg) {
+                    	//redirect input to /dev/null 
+                    	//if input file not specified
+                    	fd = open("/dev/null", O_RDONLY);
+                    	if (fd == -1) {
+				//error opening
+                        	perror("open error");
+                       		exit(1);
+                   	}
+                    	else if (dup2(fd, 0) == -1) {
+                        	perror("dup2 error");
+                       		exit(1);
+                   	}
 			
-				//Close file stream		
-				close(fd);
-                  	   }
-                	   else  if (oFile != NULL) { //If file to output to
-                    		//open specified file
-                    		fd = open(oFile, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+			//Close file stream		
+			close(fd);
+                     }
+                     else  if (oFile != NULL) { //If file to output to
+                    	//open specified file
+                    	fd = open(oFile, O_WRONLY | O_CREAT | O_TRUNC, 0644);
                     
-				if (fd == -1) {
-					//Error opening file, exit
-                        		printf("Invalid file: %s\n", oFile);
-                        		fflush(stdout);
-                       			 _Exit(1);
-                   		 }
+			if (fd == -1) {
+				//Error opening file, exit
+                        	printf("Invalid file: %s\n", oFile);
+                        	fflush(stdout);
+                       		exit(1);
+                   	}
 
-	                    	if (dup2(fd, 1) == -1) { //Redirect output
-					//Error redirecting
-        	                	perror("dup2 error");
-                	       		 _Exit(1);
-                   	    	 }
+	                if (dup2(fd, 1) == -1) { //Redirect output
+				//Error redirecting
+        	                perror("dup2 error");
+                	       	exit(1);
+                   	}
 
-				//close file stream
-                    		close(fd);
-                	}
+			//close file stream
+                    	close(fd);
+                     }
                 
-			//exec command stored in arg[0]
-                	if (execvp(args[0], args)) {
-				//Command not recognized error, exit
-                    		printf("Command not recognized: \n", args[0]);
-                    		fflush(stdout);
-                   		 _Exit(1);
-			}
+		     //exec command stored in arg[0]
+                     if (execvp(args[0], args)) {
+			//Command not recognized error, exit
+                    	printf("Command not recognized: \n", args[0]);
+                    	fflush(stdout);
+                   	exit(1);
+		      }
                  }
 		else if (pid < 0) { //fork() error
         	        perror("fork error");
